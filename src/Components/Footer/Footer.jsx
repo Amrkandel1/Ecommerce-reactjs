28837e28c66b2b6cc273da60e87c3036d83cd0f7
amrkandel1 <kandelamr417@gmail.com>
2023-10-11 13:59:17 +0300

    update on footer responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -22,45 +22,52 @@ export default function Footer() {
         <h1 className=''>Get the FreshCart app</h1>
         <p className=' text-secondary fs-5'>We will send you a link, open it on your phone to download the app</p>
 
-        <div>
-          <div className="row d-flex  text-center ps-4">
+        <div className=''>
+          <div className="row d-flex gy-3 text-center ps-4">
 
-            <div className="col-md-10">
+            <div className="col-md-9">
             <input className={`form-control ${style['form-control']}`} type="email" placeholder='Email ..' />
             </div>
 
-            <div className="col-md-2">
+            <div className="col-md-3">
               <button className='btn bg-main text-white'>Share App Link</button>             
             </div>
 
           </div>
 
-          <div className={`row ${style.borders} d-flex justify-content-between mt-5 py-1`}>
+          <div className={`row ${style.borders}  m-auto d-flex justify-content-between align-items-center mt-4 py-1`}>
 
-            <div className="col-md-6 d-flex align-items-center">
-              <h4>Payment Partners </h4>
+            <div className="col-xl-6">
+              <div className="row d-flex justify-content-start align-items-center">
+              <div className="col-xl-6 gy-3">                
+                <h4 className=''>Payment Partners :</h4>                              
+              </div>
+
+              <div className="col-xl-6 gy-3">
               <img className={`cursor-pointer ms-2 ${style.imgWidth}`} src={amazonpay} alt="" />
               <img className={`cursor-pointer ms-2 ${style.imgWidth}`} src={american} alt="" />
               <img className={`cursor-pointer ms-2 ${style.imgWidth}`} src={mastercard} alt="" />
               <img className={`cursor-pointer ms-2 ${style.imgWidth}`} src={paypal} alt="" />
+              </div>
+              </div>             
             </div>
 
-            <div className="col-md-6 justify-content-end d-flex align-items-center">
+            <div className="col-xl-6"> 
+               <div className="row d-flex justify-content-end align-items-center">
 
-              <div>
-              <h4>Get deliveries with FreshCart</h4>
-              </div>
+                <div className="col-xl-6 gy-3">
+                <h4 className=''>Download App From :</h4>
+                </div>
 
-              <div className='d-flex align-items-center'>
-              <img className={`cursor-pointer ms-2 ${style.imgWidth2}`} src={appstore} alt="" />
-              <img className={`cursor-pointer ms-2 ${style.imgWidth2}`} src={googleplay} alt="" />
-              </div>
-                           
+                <div className="col-xl-6 gy-3">
+                <img className={`cursor-pointer ${style.imgWidth2}`} src={appstore} alt="" />
+                <img className={`cursor-pointer ${style.imgWidth2}`} src={googleplay} alt="" />
+                </div>
+
+               </div>
             </div>
-
           </div>
         </div>
-
       </div>
     </div>
   </div>
